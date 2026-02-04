4ee79bcd19fd86c948febb8decb412c4a3e61d10
Vasily Postnicov <shamaz.mazum@gmail.com>
2026-02-04 23:02:30 +0300

    Use qlot

--- tests/travis.lisp
+++ tests/travis.lisp
@@ -1,5 +1,8 @@
 (defun do-all()
-  (ql:quickload :cl-wavelets/tests)
+  (handler-case
+      (asdf:load-system :cl-wavelets/tests)
+    (error ()
+      (uiop:quit 1)))
   (uiop:quit
    (if (uiop:call-function "cl-wavelets-tests:run-tests")
        0 1)))
